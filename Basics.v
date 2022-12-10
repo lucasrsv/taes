6d4ece0c48c1d70b9e6922202ccd2ae0c4c0ff17
Lucas <lucasrsvalenca@gmail.com>
2022-12-10 19:55:41 -0300

    basics: ltb

--- Basics.v
+++ Basics.v
@@ -880,17 +880,25 @@ Proof. simpl. reflexivity.  Qed.
     function.  (It can be done with just one previously defined
     function, but you can use two if you want.) *)
 
-Definition ltb (n m : nat) : bool
-  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
-
+Fixpoint ltb (n m : nat) : bool :=
+  match n with
+  | O => match m with
+         | O => false
+         | S m' => true
+         end
+  | S n' => match m with
+           | O => false
+           | S m'=> ltb n' m'
+           end
+  end.
 Notation "x <? y" := (ltb x y) (at level 70) : nat_scope.
 
 Example test_ltb1:             (ltb 2 2) = false.
-(* FILL IN HERE *) Admitted.
+Proof. simpl. reflexivity.  Qed.
 Example test_ltb2:             (ltb 2 4) = true.
-(* FILL IN HERE *) Admitted.
+Proof. simpl. reflexivity.  Qed.
 Example test_ltb3:             (ltb 4 2) = false.
-(* FILL IN HERE *) Admitted.
+Proof. simpl. reflexivity.  Qed.
 (** [] *)
 
 (* ################################################################# *)
